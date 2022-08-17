9539a3cfa68013bcc0b4dbb51af5410cafafa309
Liu Xiao <42756849+liuxiaocs7@users.noreply.github.com>
2022-08-17 23:27:11 +0800

    feat: add wordcount example (#10)
    add wordcount example

---
+++ melon-example/src/main/java/com/toutiao/melon/example/TwoReduceOperator.java
@@ -0,0 +1,51 @@
+package com.toutiao.melon.example;
+
+import com.toutiao.melon.api.IOperator;
+import com.toutiao.melon.api.stream.Collector;
+import com.toutiao.melon.api.stream.Event;
+import com.toutiao.melon.api.stream.OutGoingStream;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.TimeZone;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TwoReduceOperator implements IOperator {
+
+    Map<String, Integer> wordCountMap = new ConcurrentHashMap<>();
+    private Timer timer;
+
+    public TwoReduceOperator() {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (!wordCountMap.isEmpty()) {
+                    for (Map.Entry<String, Integer> wordCount : wordCountMap.entrySet()) {
+                        String wordKey = wordCount.getKey();
+                        int wordValue = wordCount.getValue();
+                        System.out.println("two: " + LocalDateTime.ofInstant(
+                                Instant.ofEpochMilli(System.currentTimeMillis()),
+                                TimeZone.getDefault().toZoneId())
+                                + " " + wordKey + "," + wordValue);
+                    }
+                    wordCountMap.clear();
+                }
+            }
+        }, 0, 20 * 1000);
+    }
+
+    @Override
+    public void compute(Event event, Collector collector) {
+        String word = event.getStringByName("twoWord");
+        int count = event.getIntByName("twoCount");
+        wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + count);
+    }
+
+    @Override
+    public void defineOutGoingStream(OutGoingStream outGoingStream) {
+
+    }
+}
