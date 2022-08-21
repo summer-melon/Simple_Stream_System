ede566f6633f8482b7cf79a90419e5e609daa7d2
liuxiao <liuxiao2103@qq.com>
2022-08-21 21:54:36 +0800

    fix: mock package missing data file

--- melon-mock/src/main/java/com/toutiao/melon/mock/data/MockData.java
+++ melon-mock/src/main/java/com/toutiao/melon/mock/data/MockData.java
@@ -3,6 +3,8 @@ package com.toutiao.melon.mock.data;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -14,11 +16,9 @@ public class MockData {
     public static List<String> mockData = new ArrayList<>();
 
     static {
-        String filePath = Objects.requireNonNull(
-                MockData.class.getClassLoader().getResource(MockData.FILE_NAME)).getFile();
-
-        try (FileReader reader = new FileReader(filePath);
-             BufferedReader br = new BufferedReader(reader)
+        try (InputStream in = MockData.class.getResourceAsStream("/mockData.txt");
+             InputStreamReader isr = new InputStreamReader(in);
+             BufferedReader br = new BufferedReader(isr);
         ) {
             String line;
             while ((line = br.readLine()) != null) {
